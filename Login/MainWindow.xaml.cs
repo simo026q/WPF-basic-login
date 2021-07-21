1129be41a570d7e0339f99cfe80ea7acdc0e5b12
simo026q <simonchristensen03@gmail.com>
2021-07-21 12:23:47 +0200

    Initial commit

---
+++ Login/MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Login
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private static bool signedIn = false;
+        public MainWindow()
+        {
+            InitializeComponent();
+            User.CreateValidUser("simo026q", "simonchristensen03@gmail.com", "eqq67awk");
+            DataGridUsers.ItemsSource = User.Users;
+        }
+        // Button sign in
+        private void BtnSignIn_Click(object sender, RoutedEventArgs e)
+        {
+            string inputUsername = SigninUsername.Text;
+            string inputPassword = SigninPassword.Password;
+            SigninUsername.Text = "";
+            SigninPassword.Password = "";
+            if (ValidateField(inputUsername) && ValidateField(inputPassword))
+            {
+                if (User.ValidateLogin(inputUsername, inputPassword))
+                {
+                    signedIn = true;
+                    TabUsers.Visibility = Visibility.Visible;
+                    MessageBox.Show(String.Format("Signed In as: {0}", inputUsername), "Success!", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+                else
+                {
+                    MessageBox.Show("Wrong log in :(", "Error!", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Empty field(s)", "Error!", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+        // Button sign up
+        private void BtnSignUp_Click(object sender, RoutedEventArgs e)
+        {
+            string inputUsername = SignupUsername.Text;
+            string inputEmail = SignupEmail.Text;
+            string inputPassword = SignupPassword.Password;
+            SignupUsername.Text = "";
+            SignupEmail.Text = "";
+            SignupPassword.Password = "";
+            if (ValidateField(inputUsername) && ValidateField(inputPassword))
+            {
+                MessageBoxResult confirm = MessageBox.Show(String.Format("Are you sure you want to create: {0}", inputUsername), "Are you sure?", MessageBoxButton.YesNo, MessageBoxImage.Question);
+                if (confirm == MessageBoxResult.Yes)
+                {
+                    User.CreateValidUser(inputUsername, inputEmail, inputPassword);
+                }
+            }
+        }
+        // Check if the field is empty
+        public bool ValidateField(string field)
+        {
+            return (field != "");
+        }
+    }
+}
---
+++ Login/User.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Login
+{
+    public class User
+    {
+        // List of all the users
+        public static List<User> Users { get; private set; }
+        public string Username { get; private set; }
+        public string Email { get; private set; }
+        private string Password;
+        public User(string username, string email, string password)
+        {
+            if (ValidateEmail(email)) Email = email;
+            else Email = "";
+            Username = username.ToLower();
+            Password = password;
+            if (Users == null) Users = new List<User>();
+        }
+        // Creates and validates a new user
+        public static bool CreateValidUser(string username, string email, string password)
+        {
+            if (Users == null) Users = new List<User>();
+            bool valid = true;
+            username = username.ToLower();
+            if (username == "" || password == "")
+            {
+                valid = false;
+                MessageBox.Show("The username or/and password field is empty", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return valid;
+            }
+            if (password.Length < 8)
+            {
+                valid = false;
+                MessageBox.Show("Password does not meet the requirements. The password must be at least 8 characters!", "Password Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return valid;
+            }
+            // If the email and username has not been used before
+            foreach (User user in Users)
+            {
+                if (user.Username == username || (user.Email == email && user.Email != ""))
+                {
+                    valid = false;
+                    MessageBox.Show("The username or email is already in use!", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                    return valid;
+                }
+            }
+            // If the email is not valid
+            if (!ValidateEmail(email)) email = "";
+            if (valid)
+            {
+                User user = new User(username, email, password);
+                Users.Add(user);
+            }
+            return valid;
+        }
+        // Set the users password
+        public void SetPassword(string newPassword)
+        {
+            if (newPassword != "")
+            {
+                Password = newPassword;
+            }
+        }
+        // Check if the username/email and password is valid
+        public static bool ValidateLogin(string username, string password)
+        {
+            bool valid = false;
+            foreach (User user in Users)
+            {
+                if ((user.Username == username || (user.Email == username && user.Email != "")) && user.Password == password)
+                {
+                    valid = true;
+                    continue;
+                }
+            }
+            return valid;
+        }
+        // Check if a email is in the valid format
+        public static bool ValidateEmail(string email)
+        {
+            try
+            {
+                var addr = new System.Net.Mail.MailAddress(email);
+                return addr.Address == email;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
